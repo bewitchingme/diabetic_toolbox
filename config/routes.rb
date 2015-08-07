adc2485750fc643e6b682d6f31265ef03f0303cf
bewitchingme <rpc@bewitching.me>
2015-08-07 19:41:35 -0400

    Updated routes for the 3 controllers that exist so far; locale added for en with translations current as of this commit.  FriendlyId initializer included.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 DiabeticToolbox::Engine.routes.draw do
-  devise_for :patients, class_name: "DiabeticToolbox::Patient"
+  # App home.
+  root 'welcome#start'
+
+  # Authentication routes
+  get    '/member/sign_in',  to: 'member_sessions#new'
+  post   '/member/sign_in',  to: 'member_sessions#create'
+  delete '/member/sign_out', to: 'member_sessions#destroy'
+
+  # Members self-management
+  resources :members
+
 end
