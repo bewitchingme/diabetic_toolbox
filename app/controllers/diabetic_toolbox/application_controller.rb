c80618110cc5aac11ac904f42ef1a26efb40f23c
bewitchingme <rpc@bewitching.me>
2015-08-08 17:35:00 -0400

    Since the tabs are application specific, routes to the tabs, their names and icon hints are created here until I can find a better place for them.  Also, we don't look up the user unless there's actually a session token stored in their session, bad bug to have created.

--- app/controllers/diabetic_toolbox/application_controller.rb
+++ app/controllers/diabetic_toolbox/application_controller.rb
@@ -13,9 +13,20 @@ module DiabeticToolbox
         @current_member.present?
       end
 
+    protected
+      def deploy_member_tabs
+        @tabs = {
+            :dashboard  => [I18n.t('navigation.members.dashboard'),  'dashboard', member_dash_path(current_member)],
+            :readings   => [I18n.t('navigation.members.readings'),   'book',      '#'],
+            :reports    => [I18n.t('navigation.members.reports'),    'bar-chart', '#'],
+            :recipes    => [I18n.t('navigation.members.recipes'),    'list',      '#'],
+            :meal_plans => [I18n.t('navigation.members.meal_plans'), 'road',      '#']
+        }
+      end
+
     private
       def initialize_member_session
-        @current_member ||= DiabeticToolbox::Members::Session.find session[:session_token]
+        @current_member ||= DiabeticToolbox::Members::Session.find session[:session_token] if session[:session_token].present?
       end
   end
 end
