393d94f5ee45e333b19a9d017bda45f13b8508fc
bewitchingme <rpc@bewitching.me>
2015-08-08 17:27:16 -0400

    I don't know how this migration got past my eyes the first time; time_of_date makes no sense, test_time is the estimated time or actual time the reading was taken, while meal is an enum representing which meal of the day the member considers it to be.

--- db/migrate/20150731195612_create_diabetic_toolbox_readings.rb
+++ db/migrate/20150731195612_create_diabetic_toolbox_readings.rb
@@ -3,13 +3,17 @@ class CreateDiabeticToolboxReadings < ActiveRecord::Migration
     create_table :diabetic_toolbox_readings do |t|
       t.belongs_to :member
       t.float      :glucometer_value
-      t.datetime   :time_of_date
+      t.datetime   :test_time
+      t.integer    :meal
       t.integer    :intake
 
       t.timestamps null: false
     end
 
     add_index :diabetic_toolbox_readings, :member_id
+    add_index :diabetic_toolbox_readings, :glucometer_value
+    add_index :diabetic_toolbox_readings, :test_time
+    add_index :diabetic_toolbox_readings, :meal
   end
 
   def down
