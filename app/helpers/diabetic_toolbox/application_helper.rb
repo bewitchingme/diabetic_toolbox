bbb67bb367aaade6a06ca24b650bda9c268d62a1
bewitchingme <rpc@bewitching.me>
2015-08-08 17:38:31 -0400

    To keep my views dry (and considering this will be used by multiple views) the member navigation belongs to the application.  Displaying the Gravatar for the email address provided by the member.
    I think both of these should be moved to somewhere better suited to keep the application code execution to a minimum, will think on that later.

--- app/helpers/diabetic_toolbox/application_helper.rb
+++ app/helpers/diabetic_toolbox/application_helper.rb
@@ -1,4 +1,11 @@
 module DiabeticToolbox
   module ApplicationHelper
+    def render_member_navigation(active)
+      render partial: 'diabetic_toolbox/members/member_navigation', locals: { tabs: @tabs, active: active }
+    end
+
+    def show_member_gravatar
+      image_tag "http://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(current_member.email.downcase)}?s=22&d=mm", class: 'img-circle'
+    end
   end
 end
