b4478b84b0aca3899a3d12eb66d05244c645d612
bewitchingme <rpc@bewitching.me>
2015-08-08 17:26:07 -0400

    New strings for the dashboard and a few other elements; routes added for members self-management and the dashboard.

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ DiabeticToolbox::Engine.routes.draw do
   delete '/member/sign_out', to: 'member_sessions#destroy'
 
   # Members self-management
-  resources :members
+  get    '/register',         to: 'members#new',            as: :new_member
+  post   '/register',         to: 'members#create',         as: :create_member
+  get    '/member/:id',       to: 'members#show',           as: :show_member
+  get    '/me/:id',           to: 'members#edit',           as: :edit_member
+  put    '/me/:id',           to: 'members#update',         as: :update_member
+  delete '/member/delete',    to: 'members#destroy',        as: :delete_member
+  get    '/dash/:id',         to: 'members#dash',           as: :member_dash
+  get    '/member/delete',    to: 'members#confirm_delete', as: :member_delete_confirmation
+  # resources :members
 
 end
