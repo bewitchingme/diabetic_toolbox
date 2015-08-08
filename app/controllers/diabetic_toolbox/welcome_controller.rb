cade0e1bc8c668b96b1b5526cf4ac240f47b86ab
bewitchingme <rpc@bewitching.me>
2015-08-08 17:37:01 -0400

    If a member is signed in, the root path will always redirect them to their dashboard.

--- app/controllers/diabetic_toolbox/welcome_controller.rb
+++ app/controllers/diabetic_toolbox/welcome_controller.rb
@@ -3,6 +3,7 @@ require_dependency "diabetic_toolbox/application_controller"
 module DiabeticToolbox
   class WelcomeController < DiabeticToolbox::ApplicationController
     def start
+      redirect_to member_dash_path(current_member) if member_signed_in?
     end
   end
 end
