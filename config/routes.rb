9f324836492dc486757187674dbb5952a34aafe0
bewitchingme <rpc@bewitching.me>
2015-08-09 01:20:35 -0400

    Added strings for the dashboard and authentication.  All routes are now named routes moving forward to create simplicity.

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ DiabeticToolbox::Engine.routes.draw do
   root 'welcome#start'
 
   # Authentication routes
-  get    '/member/sign_in',  to: 'member_sessions#new'
-  post   '/member/sign_in',  to: 'member_sessions#create'
-  delete '/member/sign_out', to: 'member_sessions#destroy'
+  get    '/member/sign_in',  to: 'member_sessions#new',               as: :sign_in
+  post   '/member/sign_in',  to: 'member_sessions#create',            as: :begin_session
+  delete '/member/sign_out', to: 'member_sessions#destroy',           as: :sign_out
+  get    '/member/recover',  to: 'member_sessions#password_recovery', as: :password_recovery
+  post   '/member/recover',  to: 'member_sessions#send_recovery_kit', as: :recovery_kit
 
   # Members self-management
   get    '/register',         to: 'members#new',            as: :new_member
   post   '/register',         to: 'members#create',         as: :create_member
   get    '/member/:id',       to: 'members#show',           as: :show_member
   get    '/me/:id',           to: 'members#edit',           as: :edit_member
-  put    '/me/:id',           to: 'members#update',         as: :update_member
+  match  '/me/:id',           to: 'members#update',         as: :update_member, via: [:patch, :put]
   delete '/member/delete',    to: 'members#destroy',        as: :delete_member
   get    '/dash/:id',         to: 'members#dash',           as: :member_dash
   get    '/member/delete',    to: 'members#confirm_delete', as: :member_delete_confirmation
-  # resources :members
-
 end
