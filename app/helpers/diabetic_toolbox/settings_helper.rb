d943c7c7896a046309a0c873f9952b87b7d12443
bewitchingme <rpc@bewitching.me>
2015-08-10 18:47:53 -0400

    Simplified the settings_helper and we now discern between the addon used for the increments_by field and the base correction_base field. The welcome_helper renders a partial based on locale.  Current en is the only local provided.

--- app/helpers/diabetic_toolbox/settings_helper.rb
+++ app/helpers/diabetic_toolbox/settings_helper.rb
@@ -1,7 +1,7 @@
 module DiabeticToolbox
   module SettingsHelper
     def glucometer_options(setting)
-      options = DiabeticToolbox::Setting.glucometer_measure_types.keys.map { |key| [key.to_s.titlecase, key] }
+      options = DiabeticToolbox::Setting.glucometer_options
       if setting.id
         options_for_select options, selected: setting.glucometer_measure_type
       else
@@ -10,7 +10,7 @@ module DiabeticToolbox
     end
 
     def intake_options(setting)
-      options = DiabeticToolbox::Setting.intake_measure_types.keys.map { |key| [key.to_s.titlecase, key] }
+      options = DiabeticToolbox::Setting.intake_options
       if setting.id
         options_for_select options, selected: setting.intake_measure_type
       else
@@ -25,10 +25,12 @@ module DiabeticToolbox
       end
     end
 
-    def bootstrap_addon_glucometer(setting)
+    def bootstrap_addon_glucometer(setting, addon_for_increment = false)
       unless setting.new_record?
-        return t('views.settings.mmol') if setting.glucometer_measure_type.to_sym.eql? :mmol
-        return t('views.settings.mg') if setting.glucometer_measure_type.to_sym.eql? :mg
+        return t('views.settings.mmol') if setting.mmol? && !addon_for_increment
+        return t('views.settings.mmol_units') if setting.mmol? && addon_for_increment
+        return t('views.settings.mg') if setting.mg? && !addon_for_increment
+        return t('views.settings.mg_units') if setting.mg? && addon_for_increment
       end
     end
   end
--- app/helpers/diabetic_toolbox/welcome_helper.rb
+++ app/helpers/diabetic_toolbox/welcome_helper.rb
@@ -1,4 +1,7 @@
 module DiabeticToolbox
   module WelcomeHelper
+    def about_this_toolbox
+      render partial: "diabetic_toolbox/welcome/about.#{I18n.locale.to_s}"
+    end
   end
 end
