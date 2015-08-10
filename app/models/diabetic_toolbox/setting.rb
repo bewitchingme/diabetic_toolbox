09d53d37b2c78514e58dfe3521e9b825e568afed
bewitchingme <rpc@bewitching.me>
2015-08-10 18:49:51 -0400

    Two methods added to export the options for the glucometer and intake dropdowns; I may alas move this to a helper, but for the time being it works best here.

--- app/models/diabetic_toolbox/setting.rb
+++ app/models/diabetic_toolbox/setting.rb
@@ -4,5 +4,19 @@ module DiabeticToolbox
 
     enum glucometer_measure_type: [:mmol, :mg]
     enum intake_measure_type:     [:carbohydrates, :calories]
+
+    def self.glucometer_options
+      [
+        [I18n.t('activerecord.options.diabetic_toolbox/setting.mmol'), :mmol],
+        [I18n.t('activerecord.options.diabetic_toolbox/setting.mg'), :mg]
+      ]
+    end
+
+    def self.intake_options
+      [
+        [I18n.t('activerecord.options.diabetic_toolbox/setting.carbohydrates'), :carbohydrates],
+        [I18n.t('activerecord.options.diabetic_toolbox/setting.calories'), :calories]
+      ]
+    end
   end
 end
