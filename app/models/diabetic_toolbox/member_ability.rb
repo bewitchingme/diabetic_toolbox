4431d88b62d66a69cf096e7a8dc3c563b80deacb
bewitchingme <rpc@bewitching.me>
2015-08-10 18:49:01 -0400

    Configuration for all actions that can currently be taken are now provided.  show_interest included for static pages accessible by all.

--- app/models/diabetic_toolbox/member_ability.rb
+++ app/models/diabetic_toolbox/member_ability.rb
@@ -4,13 +4,20 @@ class DiabeticToolbox::MemberAbility
   def initialize(member)
     @member = member
 
+    show_interest
     self_manage if @member.present?
   end
 
   private
+    def show_interest
+      can [:new, :create],  DiabeticToolbox::Member
+      can [:start, :about], :welcome
+      can [:new],           :member_sessions
+    end
+
     def self_manage
       can [:manage, :dash], DiabeticToolbox::Member,  id:        @member.id
       can [:manage],        DiabeticToolbox::Setting, member_id: @member.id
-      can [:destroy],       :member_session,          member_id: @member.id
+      can [:destroy],       :member_sessions,         member_id: @member.id
     end
 end
