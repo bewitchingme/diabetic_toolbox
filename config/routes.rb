f5d9c4cbf79e77ae195d9e020aa1abeaa4360627
bewitchingme <rpc@bewitching.me>
2015-08-10 04:33:36 -0400

    Routes added for settings.

--- config/routes.rb
+++ config/routes.rb
@@ -10,12 +10,19 @@ DiabeticToolbox::Engine.routes.draw do
   post   '/member/recover',  to: 'member_sessions#send_recovery_kit', as: :recovery_kit
 
   # Members self-management
-  get    '/register',         to: 'members#new',            as: :new_member
-  post   '/register',         to: 'members#create',         as: :create_member
-  get    '/member/:id',       to: 'members#show',           as: :show_member
-  get    '/me/:id',           to: 'members#edit',           as: :edit_member
-  match  '/me/:id',           to: 'members#update',         as: :update_member, via: [:patch, :put]
-  delete '/member/delete',    to: 'members#destroy',        as: :delete_member
-  get    '/dash/:id',         to: 'members#dash',           as: :member_dash
-  get    '/member/delete',    to: 'members#confirm_delete', as: :member_delete_confirmation
+  get    '/register',      to: 'members#new',            as: :new_member
+  post   '/register',      to: 'members#create',         as: :create_member
+  get    '/member/:id',    to: 'members#show',           as: :show_member
+  get    '/me/:id',        to: 'members#edit',           as: :edit_member
+  match  '/me/:id',        to: 'members#update',         as: :update_member, via: [:patch, :put]
+  delete '/member/delete', to: 'members#destroy',        as: :destroy_member
+  get    '/dash/:id',      to: 'members#dash',           as: :member_dashboard
+  get    '/member/delete', to: 'members#confirm_delete', as: :last_chance
+
+  # Member settings
+  get    '/setup',    to: 'settings#new',    as: :setup
+  post   '/setup',    to: 'settings#create', as: :create_setting
+  get    '/settings', to: 'settings#edit',   as: :settings
+  match  '/settings', to: 'settings#update', as: :update_settings, via: [:patch, :put]
+
 end
