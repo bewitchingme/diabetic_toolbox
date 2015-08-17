91760f306e53a8fd35e8156b49fb0f6812b60ace
bewitchingme <rpc@bewitching.me>
2015-08-17 03:53:46 -0400

    Added regions for this model and removed AR < version 4 support (not needed as the engine requires Rails 4.2.3 anyway)

--- app/models/diabetic_toolbox/vote.rb
+++ app/models/diabetic_toolbox/vote.rb
@@ -1,16 +1,17 @@
 module DiabeticToolbox
   class Vote < ActiveRecord::Base
-
+    #region Scopes
     scope :for_voter, lambda { |*args| where(["voter_id = ? AND voter_type = ?", args.first.id, args.first.class.base_class.name]) }
     scope :for_voteable, lambda { |*args| where(["voteable_id = ? AND voteable_type = ?", args.first.id, args.first.class.base_class.name]) }
     scope :recent, lambda { |*args| where(["created_at > ?", (args.first || 2.weeks.ago)]) }
     scope :descending, lambda { order("created_at DESC") }
 
+    validates_uniqueness_of :voteable_id, :scope => [:voteable_type, :voter_type, :voter_id]
+    #endregion
+
+    #region Associations
     belongs_to :voteable, :polymorphic => true
     belongs_to :voter, :polymorphic => true
-
-    attr_accessible :vote, :voter, :voteable if ActiveRecord::VERSION::MAJOR < 4
-
-    validates_uniqueness_of :voteable_id, :scope => [:voteable_type, :voter_type, :voter_id]
+    #endregion
   end
 end
