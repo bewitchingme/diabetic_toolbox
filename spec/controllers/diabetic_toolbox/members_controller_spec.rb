a2a81d1daa4e6b5fad10fb154af9c4203faeefc3
bewitchingme <rpc@bewitching.me>
2015-08-17 02:49:47 -0400

    Initial tests for controllers

--- spec/controllers/diabetic_toolbox/members_controller_spec.rb
+++ spec/controllers/diabetic_toolbox/members_controller_spec.rb
@@ -2,6 +2,30 @@ require 'rails_helper'
 
 module DiabeticToolbox
   RSpec.describe MembersController, type: :controller do
+    routes { DiabeticToolbox::Engine.routes }
+    let(:member) { build(:member) }
 
+    describe 'a member' do
+      context 'who is authenticated without configured settings' do
+        it 'should be redirected to setup_path from :dash' do
+          sign_in_member member
+          get :dash
+
+          expect(response).to redirect_to setup_path
+        end
+      end
+
+      context 'who is authenticated with configured settings' do
+        it 'should have 200 HTTP status for :dash' do
+          setting = build(:setting)
+          member.settings << setting
+          sign_in_member member
+
+          get :dash
+
+          expect(response).to have_http_status(200)
+        end
+      end
+    end
   end
 end
--- spec/controllers/diabetic_toolbox/welcome_controller_spec.rb
+++ spec/controllers/diabetic_toolbox/welcome_controller_spec.rb
@@ -2,6 +2,30 @@ require 'rails_helper'
 
 module DiabeticToolbox
   RSpec.describe WelcomeController, type: :controller do
+    #region Definitions
+    routes { DiabeticToolbox::Engine.routes }
+    #endregion
 
+    #region Static Routes
+    describe 'GET root_path' do
+      context 'as defined by route /' do
+        it 'responds with 200 status' do
+          get :start
+
+          expect(response).to have_http_status(200)
+        end
+      end
+    end
+
+    describe 'GET about_path' do
+      context 'as defined by route /about' do
+        it 'responds with 200 status' do
+          get :about
+
+          expect(response).to have_http_status(200)
+        end
+      end
+    end
+    #endregion
   end
 end
