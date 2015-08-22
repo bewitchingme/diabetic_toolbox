09eed9cc58f33d834b0e988e2c8c73a6692f6e3c
bewitchingme <rpc@bewitching.me>
2015-08-22 04:17:25 -0400

    Namespace removed.

--- spec/factories/member.rb
+++ spec/factories/member.rb
@@ -2,7 +2,7 @@ require 'faker'
 
 module DiabeticToolbox
   FactoryGirl.define do
-    factory :members, class: DiabeticToolbox::Member do |member|
+    factory :members, class: Member do |member|
       password = Faker::Internet.password(8, 64)
       sequence(:email) { |n| Faker::Internet.email "#{Faker::Name.first_name}#{n}" }
       member.first_name Faker::Name.first_name
@@ -13,7 +13,7 @@ module DiabeticToolbox
       member.password_confirmation password
     end
 
-    factory :member, class: DiabeticToolbox::Member do |member|
+    factory :member, class: Member do |member|
       member.sequence(:email) { |n| "frodo.baggins#{n}@example.com" }
       member.first_name 'Frodo'
       member.last_name 'Baggins'
