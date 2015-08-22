4d4f7aac0cf996818289cce4e2e7e593f6a86d04
bewitchingme <rpc@bewitching.me>
2015-08-22 04:18:10 -0400

    Will now clean the database before the suite runs.

--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -1,8 +1,13 @@
 RSpec.configure do |config|
   config.include FactoryGirl::Syntax::Methods
+
   config.before(:suite) do
     begin
+      DatabaseCleaner.strategy = :transaction
+      DatabaseCleaner.clean_with :truncation
+
       DatabaseCleaner.start
+      DatabaseCleaner.clean
       FactoryGirl.lint
     ensure
       DatabaseCleaner.clean
