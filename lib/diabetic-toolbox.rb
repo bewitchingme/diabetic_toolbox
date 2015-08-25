100858f3e5f2b3dbdd0ad45f09c2dd4b4a93d59f
bewitchingme <rpc@bewitching.me>
2015-08-25 19:37:11 -0400

    - DiabeticToolbox.from now determines that a requirement has been supplied. - @@me is used to identify the module namespace as it's used on disk - @@safe will now contain safe model fields that can be returned from a model to the UI instead of the constants previously declared in Action classes. - DiabeticToolbox.rely_on is used to require classes in the namespace related to infrastructure for other POROs - DiabeticToolbox.safe used to extract safe model attributes from @@safe

--- lib/diabetic-toolbox.rb
+++ lib/diabetic-toolbox.rb
@@ -1,3 +1,5 @@
+
+#region Requirements
 require 'warden'
 require 'diabetic_toolbox/engine'
 require 'sessions/session'
@@ -16,13 +18,39 @@ require 'chartkick'
 require 'momentjs-rails'
 require 'bootstrap3-datetimepicker-rails'
 require 'prawn-rails'
+#endregion
 
+#region DiabeticToolbox
 module DiabeticToolbox
-  def self.from(scope, options)
+  #region Module Fields
+  @@me   = :diabetic_toolbox
+  @@safe = {
+      member: [:first_name, :last_name, :username, :slug]
+  }
+  #endregion
+
+  #region Data Response Safeties
+  def self.safe(model)
+    @@safe[model]
+  end
+  #endregion
+
+  #region Granularity for Requirements
+  def self.from(scope, options = {})
     if options.has_key? :require
       options[:require].each do |requirement|
-        require "#{Engine.root}/app/actions/diabetic_toolbox/#{scope.to_s}/#{requirement}"
+        require Engine.root.join 'app', 'actions', @@me.to_s, scope.to_s, requirement
       end
     end
   end
+
+  def self.rely_on(*args)
+    if args.length > 0
+      args.each do |requirement|
+        require Engine.root.join 'app', 'actions', @@me.to_s, requirement.to_s
+      end
+    end
+  end
+  #endregion
 end
+#endregion
