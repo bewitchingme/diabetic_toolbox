08a262d6c2b12339b0c0920c3a6fc6887cd14da6
bewitchingme <rpc@bewitching.me>
2015-08-24 21:33:25 -0400

    Let Rails do this work.

--- app/helpers/diabetic_toolbox/welcome_helper.rb
+++ app/helpers/diabetic_toolbox/welcome_helper.rb
@@ -1,7 +1,7 @@
 module DiabeticToolbox
   module WelcomeHelper
     def about_this_toolbox
-      render partial: "diabetic_toolbox/welcome/about.#{I18n.locale.to_s}"
+      render partial: "diabetic_toolbox/welcome/about"
     end
   end
 end
