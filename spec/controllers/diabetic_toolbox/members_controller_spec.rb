1a6191b48ab1273fee8d3246e689db9991cc1d7f
bewitchingme <rpc@bewitching.me>
2015-09-01 00:26:45 -0400

    - Now filling in the blanks on this spec.

--- spec/controllers/diabetic_toolbox/members_controller_spec.rb
+++ spec/controllers/diabetic_toolbox/members_controller_spec.rb
@@ -2,20 +2,24 @@ require 'rails_helper'
 
 module DiabeticToolbox
   RSpec.describe MembersController, type: :controller do
+    #region Settings
     routes { DiabeticToolbox::Engine.routes }
-    let(:member) { build(:member) }
+    #endregion
 
+    #region Member
     describe 'a member' do
-      context 'who is authenticated without configured settings' do
+      let(:member) { build(:member) }
+      context 'without configured settings' do
         it 'should be redirected to setup_path from :dash' do
           sign_in_member member
           get :dash
 
+          expect(response).to have_http_status 302
           expect(response).to redirect_to setup_path
         end
       end
 
-      context 'who is authenticated with configured settings' do
+      context 'with configured settings' do
         it 'should have 200 HTTP status for :dash' do
           setting = build(:setting)
           member.settings << setting
@@ -23,9 +27,38 @@ module DiabeticToolbox
 
           get :dash
 
-          expect(response).to have_http_status(200)
+          expect(response).to have_http_status 200
+          expect(assigns(:library)).to eq nil
+          expect(assigns(:chart_data)).to eq nil
+        end
+
+        it 'should have 200 HTTP status for :edit' do
+          sign_in_member member
+
+          get :edit, id: member.slug
+
+          expect(response).to have_http_status 200
+          expect(assigns(:member)).to be_a Member
         end
       end
     end
+    #endregion
+
+    #region Visitor
+    describe 'a visitor' do
+      it 'should have 200 HTTP status for :new' do
+        get :new
+
+        expect(response).to have_http_status 200
+      end
+
+      it 'should POST to :create with appropriate parameters' do
+        post :create, member: {first_name: 'Tom', last_name: 'Bosley', username: 'Bozman', email: 'bosley@manifesto.net', password: 'password', password_confirmation: 'password', accepted_tos: true}
+
+        expect(response).to have_http_status 302
+        expect(response).to redirect_to setup_path
+      end
+    end
+    #endregion
   end
 end
