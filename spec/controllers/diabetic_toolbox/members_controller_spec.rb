5bf644d7f586fa2e08fa55fbaae91774309a83ca
bewitchingme <rpc@bewitching.me>
2015-09-01 03:35:52 -0400

    Tests now divided for visitors and members and are now appropriately implemented.

--- spec/controllers/diabetic_toolbox/members_controller_spec.rb
+++ spec/controllers/diabetic_toolbox/members_controller_spec.rb
@@ -40,6 +40,37 @@ module DiabeticToolbox
           expect(response).to have_http_status 200
           expect(assigns(:member)).to be_a Member
         end
+
+        it 'should have 200 HTTP status for :confirm_delete' do
+          sign_in_member member
+
+          get :confirm_delete, id: member.slug
+
+          expect(response).to have_http_status 200
+        end
+
+        it 'should have 302 HTTP status for :update' do
+          sign_in_member member
+
+          put :update, id: member.slug, member: {first_name: 'Doris'}
+
+          updated_member = Member.find member.slug
+
+          expect(response).to have_http_status 302
+          expect(response).to redirect_to edit_member_path member
+          expect(updated_member.first_name).to eq 'Doris'
+        end
+
+        it 'should have 302 HTTP status for :destroy' do
+          sign_in_member member
+
+          delete :destroy, id: member.slug
+
+          expect { Member.find member.slug }.to raise_error ActiveRecord::RecordNotFound
+
+          expect(response).to have_http_status 302
+          expect(response).to redirect_to root_path
+        end
       end
     end
     #endregion
@@ -57,6 +88,48 @@ module DiabeticToolbox
 
         expect(response).to have_http_status 302
         expect(response).to redirect_to setup_path
+      end
+
+      it 'should be slammed when visiting :dash' do
+        expect { get :dash }.to raise_error CanCan::AccessDenied
+      end
+
+      it 'should be slammed when visiting :edit' do
+        member = build(:member)
+        member.save
+
+        expect { get :edit, id: member.slug }.to raise_error CanCan::AccessDenied
+      end
+
+      it 'should be slammed when trying to update a member' do
+        member = build(:member)
+        member.save
+
+        update_params = {
+          password: 'password',
+          password_confirmation: 'password',
+          first_name: 'Joo Been',
+          last_name: 'Hacked'
+        }
+
+        expect {
+          put :update, id: member.slug, member: update_params
+        }.to raise_error CanCan::AccessDenied
+
+        expect {
+          patch :update, id: member.slug, member: update_params
+        }.to raise_error CanCan::AccessDenied
+      end
+
+      it 'should be slammed when trying to delete a member' do
+        member = build(:member)
+        member.save
+
+        expect { delete :destroy, id: member.slug }.to raise_error CanCan::AccessDenied
+      end
+
+      it 'should be slammed when trying to :confirm_delete' do
+        expect { get :confirm_delete }.to raise_error CanCan::AccessDenied
       end
     end
     #endregion
