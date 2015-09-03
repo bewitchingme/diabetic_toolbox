dc78e197db61d08ae7bc919ba6ee5961136047a1
bewitchingme <rpc@bewitching.me>
2015-09-03 01:02:53 -0400

    Fixed setup

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,8 +4,12 @@ require File.expand_path("../dummy/config/environment.rb",  __FILE__)
 
 require 'warden'
 require 'rspec/rails'
-require 'factory_girl_rails'
+require 'rspec/autorun'
+require 'shoulda-matchers'
+require 'capybara/rails'
+require 'capybara/rspec'
 require 'database_cleaner'
+require 'factory_girl_rails'
 require 'faker'
 require 'digest'
 
@@ -18,12 +22,20 @@ Rails.backtrace_cleaner.remove_silencers!
 # Load support files
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
 
+DatabaseCleaner.strategy = :truncation
+
 RSpec.configure do |config|
   config.include Warden::Test::ControllerHelpers, type: :controller
+  config.include DiabeticToolbox::Engine.routes.url_helpers
   config.mock_with :rspec
   config.use_transactional_fixtures = true
   config.infer_base_class_for_anonymous_controllers = false
-  config.order = "random"
+
+  config.after(:each, :type => :request) do
+    DatabaseCleaner.clean       # Truncate the database
+    Capybara.reset_sessions!    # Forget the (simulated) browser state
+    Capybara.use_default_driver # Revert Capybara.current_driver to Capybara.default_driver
+  end
 
   def sign_in_member(member)
     member.save if member.new_record?
