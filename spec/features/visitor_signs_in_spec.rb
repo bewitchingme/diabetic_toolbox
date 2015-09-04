17f9575b2cefea72a1eaaa28b8bf831c257f8982
bewitchingme <rpc@bewitching.me>
2015-09-04 19:24:12 -0400

    Tests for two sign_in results.

--- spec/features/visitor_signs_in_spec.rb
+++ spec/features/visitor_signs_in_spec.rb
@@ -1,15 +1,39 @@
 require 'spec_helper'
 
 RSpec.describe 'Visitor Signs In', type: :feature do
-  let(:member) { create(:member) }
+  #region Definitions
+  let(:member)  { create(:member) }
+  let(:setting) { build(:setting) }
+  #endregion
+
   it 'should display the username regardless of route on successful login' do
+    sign_in
+
+    expect(page).to have_text member.username
+  end
+
+  it 'should display setup dialog if no settings have been configured' do
+    sign_in
+
+    expect(page).to have_text 'Measurement Systems'
+    expect(page).to have_text 'Treatment Values'
+  end
+
+  it 'should display dashboard if settings have been configured' do
+    member.settings.create glucometer_measure_type: setting.glucometer_measure_type, intake_measure_type: setting.intake_measure_type
+    sign_in
+
+    ['Summary', 'Readings', 'Reports', 'Recipes', 'Meal Plans'].each do |chunk|
+      expect(page).to have_text chunk
+    end
+  end
+
+  def sign_in
     visit sign_in_path
 
-    fill_in 'Email', with: member.email
+    fill_in 'Email',    with: member.email
     fill_in 'Password', with: 'password'
 
     click_button 'Sign In'
-
-    expect(page).to have_content member.username
   end
 end
