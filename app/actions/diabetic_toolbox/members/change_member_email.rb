98d0e1dce6cfa0526cfe46d881fee2da71398ca2
bewitchingme <rpc@bewitching.me>
2015-09-05 02:12:58 -0400

    An action to set the unconfirmed email address when a member chooses to change their email.  This is not complete.

---
+++ app/actions/diabetic_toolbox/members/change_member_email.rb
@@ -0,0 +1,44 @@
+module DiabeticToolbox
+  rely_on :action
+
+  # = ChangeMemberEmail
+  #
+  # This action allows for the modification of a Member email address and is used as follows:
+  #
+  #   result = UpdateMember.new(member_id, params).call
+  #
+  #   if result.success?
+  #     # Success
+  #   else
+  #     # Failure
+  #   end
+  #
+  class ChangeMemberEmail < Action
+    def initialize(member_id, member_params)
+      super member_params
+      @member = Member.find(member_id)
+    end
+
+    protected
+    def _call
+      if @member.update @params
+        success do |option|
+          option[:message] = I18n.t('flash.member.updated_email.success')
+          option[:subject] = @member
+        end
+      else
+        failure do |option|
+          option[:message] = I18n.t('flash.member.updated_email.failure')
+          option[:subject] = @member
+        end
+      end
+    end
+
+    def _after_call
+      if result.success?
+        # TODO: Record was updated, now we send a confirmation email
+        # TODO: with a hash to complete this process
+      end
+    end
+  end
+end
