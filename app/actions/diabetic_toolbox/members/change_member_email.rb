394502950c0f04faacd454c84323a98101e8832a
bewitchingme <rpc@bewitching.me>
2015-09-09 20:57:09 -0400

    ChangeMemberEmail implemented aside from it's mailer

--- app/actions/diabetic_toolbox/members/change_member_email.rb
+++ app/actions/diabetic_toolbox/members/change_member_email.rb
@@ -1,3 +1,5 @@
+require 'digest'
+
 module DiabeticToolbox
   rely_on :action
 
@@ -15,21 +17,30 @@ module DiabeticToolbox
   #
   class ChangeMemberEmail < Action
     #:enddoc:
+    #region Init
     def initialize(member_id, member_params)
       super member_params
       @member = Member.find(member_id)
+      @in_use = Member.exists? email: @params[:unconfirmed_email]
     end
+    #endregion
 
+    #region Protected
     protected
     def _call
-      if @member.update @params
+      @params.merge! confirmation_values
+      change_request_successful = false
+      change_request_successful = @member.update @params unless email_in_use?
+
+      if change_request_successful
         success do |option|
           option.message = I18n.t('flash.member.updated_email.success')
           option.subject = @member
         end
       else
         failure do |option|
-          option.message = I18n.t('flash.member.updated_email.failure')
+          option.message = I18n.t('flash.member.updated_email.failure') unless email_in_use?
+          option.message = I18n.t('flash.member.updated_email.taken') if email_in_use?
           option.subject = @member
         end
       end
@@ -37,10 +48,27 @@ module DiabeticToolbox
 
     def _after_call
       if call_result.success?
-        # TODO: Record was updated, now we send a confirmation email
-        # TODO: with a hash assigned to confirmation_token to complete
-        # TODO: this process.
+        # TODO: Mailer here
       end
     end
+    #endregion
+
+    #region Private
+    private
+    def confirmation_values
+      {confirmation_token: ChangeMemberEmail.create_token, confirmation_sent_at: Time.now}
+    end
+
+    def email_in_use?
+      @in_use
+    end
+    #endregion
+
+    #region Static
+    public
+    def self.create_token
+      Digest::SHA2.hexdigest Time.now.to_s
+    end
+    #endregion
   end
 end
