aa43e7f0b8405f1650286196ff47b28706c80b4d
bewitchingme <rpc@bewitching.me>
2015-09-11 01:04:34 -0400

    Implementation complete.

--- app/mailers/diabetic_toolbox/change_member_email_mailer.rb
+++ app/mailers/diabetic_toolbox/change_member_email_mailer.rb
@@ -1,7 +1,23 @@
+require 'mail'
+
 module DiabeticToolbox
   class ChangeMemberEmailMailer < ApplicationMailer
+    #region Payload
     def send_confirmation_link(member)
       @member = member
+      if @member.present?
+        mail to: recipient.format, subject: I18n.t('mailers.change_member_email.subject')
+      end
     end
+    #endregion
+
+    #region Private
+    private
+    def recipient
+      to = Mail::Address.new @member.unconfirmed_email
+      to.display_name = "#{@member.first_name} #{@member.last_name}"
+      to
+    end
+    #endregion
   end
 end
