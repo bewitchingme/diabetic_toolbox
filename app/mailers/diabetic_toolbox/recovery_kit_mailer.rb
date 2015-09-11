965c32ad4c45e995239aed4cb45b94862b12677a
bewitchingme <rpc@bewitching.me>
2015-09-11 04:02:20 -0400

    RecoveryKitMailer now passing, yay!

--- app/mailers/diabetic_toolbox/recovery_kit_mailer.rb
+++ app/mailers/diabetic_toolbox/recovery_kit_mailer.rb
@@ -1,8 +1,21 @@
 module DiabeticToolbox
   class RecoveryKitMailer < ApplicationMailer
+    require 'mail'
+
+    #region Mailmen
     def send_forgot_password_kit(member)
       @member = member
-
+      mail to: recipient.format, subject: I18n.t('mailers.recovery_kit.subject')
     end
+    #endregion
+
+    #region Private
+    private
+    def recipient
+      to = Mail::Address.new @member.email
+      to.display_name = "#{@member.first_name} #{@member.last_name}"
+      to
+    end
+    #endregion
   end
 end
