8371de0740aa68582967b1895627fff850a752d4
bewitchingme <rpc@bewitching.me>
2015-09-12 16:56:06 -0400

    Updated initializer; now contains max_attempts (for account locking) and remember_for (the duration for which an account is remembered.)

--- spec/dummy/config/initializers/diabetic-toolbox.rb
+++ spec/dummy/config/initializers/diabetic-toolbox.rb
@@ -1,3 +1,5 @@
 DiabeticToolbox.config do |setting|
   setting.mailer_from_address = 'test.account@diabetictoolbox.org'
+  setting.max_attempts        = 3
+  setting.remember_for        = 3.months.from_now
 end
