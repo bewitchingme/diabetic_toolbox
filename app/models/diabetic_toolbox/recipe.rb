7d516b7785a842051dfa45953c85c029bf51567b
bewitchingme <rpc@bewitching.me>
2015-09-14 03:24:14 -0400

    Missing 'steps' association

--- app/models/diabetic_toolbox/recipe.rb
+++ app/models/diabetic_toolbox/recipe.rb
@@ -4,5 +4,6 @@ module DiabeticToolbox
 
     belongs_to :member,      class_name: 'DiabeticToolbox::Member',    counter_cache: true
     has_many   :ingredients, class_name: 'DiabeticToolbox::Ingredient'
+    has_many   :steps,       class_name: 'DiabeticToolbox::Step'
   end
 end
