043dc50bf5fd0b10beea779877c53c6b10066587
bewitchingme <rpc@bewitching.me>
2015-09-17 00:58:45 -0400

    A factory for recipe tests and the first success test.

---
+++ spec/factories/recipe.rb
@@ -0,0 +1,15 @@
+module DiabeticToolbox
+  FactoryGirl.define do
+    factory :brace_of_coneys, class: DiabeticToolbox::Recipe do |recipe|
+      recipe.name      'Brace of Coneys'
+      recipe.servings  4
+      recipe.published false
+    end
+
+    factory :recipe, class: DiabeticToolbox::Recipe do |recipe|
+      recipe.sequence(:name) { |n| "Foody Food#{n}" }
+      recipe.servings Random.rand(1..4)
+      recipe.published false
+    end
+  end
+end
--- spec/models/diabetic_toolbox/recipe_spec.rb
+++ spec/models/diabetic_toolbox/recipe_spec.rb
@@ -2,6 +2,29 @@ require 'rails_helper'
 
 module DiabeticToolbox
   RSpec.describe Recipe, type: :model do
-    pending "add some examples to (or delete) #{__FILE__}"
+    #region Definitions
+    let(:recipe) { build(:recipe) }
+    #endregion
+
+    describe 'a recipe' do
+      #region Creation
+      context 'being created using action class' do
+        #region Success
+        it 'should be saved with appropriate parameters' do
+          DiabeticToolbox.from :recipes, require: %w(create_recipe)
+          member = create(:member)
+          recipe_params = {
+              name: recipe.name,
+              servings: recipe.servings
+          }
+          result = CreateRecipe.new( member, recipe_params ).call
+
+          expect(result.success?).to eq true
+          expect(result.actual.published?).to eq false
+        end
+        #endregion
+      end
+      #endregion
+    end
   end
 end
