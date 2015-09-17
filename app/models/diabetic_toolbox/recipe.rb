ed9db6ac0b4cd7003bb6513565fca93e0b084bf6
bewitchingme <rpc@bewitching.me>
2015-09-17 00:38:31 -0400

    Recipe has a new child

--- app/models/diabetic_toolbox/recipe.rb
+++ app/models/diabetic_toolbox/recipe.rb
@@ -2,8 +2,9 @@ module DiabeticToolbox
   class Recipe < ActiveRecord::Base
     include DiabeticToolbox::Concerns::Voteable
 
-    belongs_to :member,      class_name: 'DiabeticToolbox::Member',    counter_cache: true
-    has_many   :ingredients, class_name: 'DiabeticToolbox::Ingredient'
-    has_many   :steps,       class_name: 'DiabeticToolbox::Step'
+    belongs_to :member,            class_name: 'DiabeticToolbox::Member', counter_cache: true
+    has_many   :ingredients,       class_name: 'DiabeticToolbox::Ingredient'
+    has_many   :steps,             class_name: 'DiabeticToolbox::Step'
+    has_many   :nutritional_facts, class_name: 'DiabeticToolbox::NutritionalFact'
   end
 end
