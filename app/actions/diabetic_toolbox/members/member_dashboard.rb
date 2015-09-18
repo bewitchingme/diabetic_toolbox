077b781ef8ffe58558d43b1e103f5f6f065e6443
bewitchingme <rpc@bewitching.me>
2015-09-18 12:38:55 -0400

    Small fix for a TypeError

--- app/actions/diabetic_toolbox/members/member_dashboard.rb
+++ app/actions/diabetic_toolbox/members/member_dashboard.rb
@@ -2,7 +2,7 @@ module DiabeticToolbox
   class MemberDashboard
     def self.history(member, travel = (Time.now - 60.days))
       history = member.readings.where(test_time: travel..(Time.now)).order(test_time: :asc)
-      history.to_a.each_with_object({}){ |r,h| h[Time.zone.parse(r.test_time)] = r.glucometer_value }
+      history.to_a.each_with_object({}){ |r,h| h[Time.zone.parse(r.test_time.to_s)] = r.glucometer_value }
     end
 
     def self.chartkick_library
