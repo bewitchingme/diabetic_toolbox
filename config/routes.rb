e0736a032e280b192a128f4b15a1fdf4973a44eb
bewitchingme <rpc@bewitching.me>
2015-09-20 20:14:38 -0400

    Modified routes; this layout makes much more sense than the last.

--- config/routes.rb
+++ config/routes.rb
@@ -7,27 +7,27 @@ DiabeticToolbox::Engine.routes.draw do
   #endregion
 
   #region Authentication
-  get    '/member/sign_in',          to: 'member_sessions#new',               as: :sign_in
-  post   '/member/sign_in',          to: 'member_sessions#create',            as: :begin_session
-  delete '/member/sign_out',         to: 'member_sessions#destroy',           as: :sign_out
-  get    '/member/request_recovery', to: 'member_sessions#password_recovery', as: :password_recovery
-  post   '/member/request_recovery', to: 'member_sessions#send_recovery_kit', as: :recovery_kit
-  get    '/member/recover/:token',   to: 'member_sessions#recover',           as: :recover_membership
-  post   '/member/recover/:token',   to: 'member_sessions#release',           as: :release_membership
-  get    '/change_email',            to: 'member_sessions#edit_email',        as: :edit_member_email
-  match  '/change_email',            to: 'member_sessions#update_email',      as: :update_member_email, via: [:patch, :put]
-  get    '/member/reconfirm/:token', to: 'member_sessions#reconfirm',         as: :reconfirmation
+  get    '/sign_in',          to: 'member_sessions#new',               as: :sign_in
+  post   '/sign_in',          to: 'member_sessions#create',            as: :begin_session
+  delete '/sign_out',         to: 'member_sessions#destroy',           as: :sign_out
+  get    '/request_recovery', to: 'member_sessions#password_recovery', as: :password_recovery
+  post   '/request_recovery', to: 'member_sessions#send_recovery_kit', as: :recovery_kit
+  get    '/recover/:token',   to: 'member_sessions#recover',           as: :recover_membership
+  post   '/recover/:token',   to: 'member_sessions#release',           as: :release_membership
+  get    '/change_email',     to: 'member_sessions#edit_email',        as: :edit_member_email
+  match  '/change_email',     to: 'member_sessions#update_email',      as: :update_member_email, via: [:patch, :put]
+  get    '/reconfirm/:token', to: 'member_sessions#reconfirm',         as: :reconfirmation
   #endregion
 
   #region Members Self-Management
-  get    '/register',                to: 'members#new',            as: :new_member
-  post   '/register',                to: 'members#create',         as: :create_member
-  get    '/members/:id',             to: 'members#show',           as: :show_member
-  get    '/me/:id',                  to: 'members#edit',           as: :edit_member
-  match  '/me/:id',                  to: 'members#update',         as: :update_member, via: [:patch, :put]
-  get    '/dash/summary',            to: 'members#dash',           as: :member_dashboard
-  delete '/membership/cancel',       to: 'members#destroy',        as: :destroy_member
-  get    '/membership/cancel',       to: 'members#confirm_delete', as: :last_chance
+  get    '/register',          to: 'members#new',            as: :new_member
+  post   '/register',          to: 'members#create',         as: :create_member
+  get    '/members/:id',       to: 'members#show',           as: :show_member
+  get    '/me',                to: 'members#edit',           as: :edit_member
+  match  '/me',                to: 'members#update',         as: :update_member, via: [:patch, :put]
+  get    '/dash',              to: 'members#dash',           as: :member_dashboard
+  delete '/membership/cancel', to: 'members#destroy',        as: :destroy_member
+  get    '/membership/cancel', to: 'members#confirm_delete', as: :last_chance
   #endregion
 
   #region Settings for members
@@ -38,19 +38,19 @@ DiabeticToolbox::Engine.routes.draw do
   #endregion
 
   #region Readings for members
-  get  '/dash/readings', to: 'readings#index',  as: :list_readings
-  get  '/dash/record',   to: 'readings#new',    as: :record_reading
-  post '/dash/record',   to: 'readings#create', as: :create_reading
+  get  '/readings', to: 'readings#index',  as: :list_readings
+  get  '/record',   to: 'readings#new',    as: :record_reading
+  post '/record',   to: 'readings#create', as: :create_reading
   #endregion
 
   #region Recipes
-  get    '/dash/recipes',             to: 'recipes#index',    as: :recipes
-  get    '/dash/recipes/add',         to: 'recipes#new',      as: :new_recipe
-  get    '/dash/recipe/:id',          to: 'recipes#edit',     as: :edit_recipe
-  get    '/recipes/:id',              to: 'recipes#show',     as: :show_recipe
-  post   '/dash/recipes/add',         to: 'recipes#create',   as: :create_recipe
-  match  '/dash/recipe/:id',          to: 'recipes#update',   as: :update_recipe,   via: [:patch, :put]
-  match  '/dash/recipe/:id/finalize', to: 'recipes#finalize', as: :finalize_recipe, via: [:patch, :put]
-  delete '/dash/recipe/:id/delete',   to: 'recipes#destroy',  as: :delete_recipe
+  get    '/recipes',             to: 'recipes#index',    as: :recipes
+  get    '/recipes/add',         to: 'recipes#new',      as: :new_recipe
+  post   '/recipes/add',         to: 'recipes#create',   as: :create_recipe
+  get    '/recipe/:id',          to: 'recipes#show',     as: :show_recipe
+  get    '/recipe/edit/:id',     to: 'recipes#edit',     as: :edit_recipe
+  match  '/recipe/edit/:id',     to: 'recipes#update',   as: :update_recipe,   via: [:patch, :put]
+  match  '/recipe/:id/finalize', to: 'recipes#finalize', as: :finalize_recipe, via: [:patch, :put]
+  delete '/recipe/:id/delete',   to: 'recipes#destroy',  as: :delete_recipe
   #endregion
 end
