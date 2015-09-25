3a9aca106c548fb207ce7f69caf961007d9c6a8a
bewitchingme <rpc@bewitching.me>
2015-09-25 04:02:41 -0400

    Destroy now implemented.

---
+++ app/actions/diabetic_toolbox/recipes/destroy_recipe.rb
@@ -0,0 +1,61 @@
+module DiabeticToolbox
+  rely_on :action
+
+  class DestroyRecipe < Action
+    #region Init
+    def initialize(member, recipe)
+      @member = member
+      @recipe = recipe
+    end
+    #endregion
+
+    #region Protected
+    def _call
+      if can_destroy?
+        if @recipe.destroy
+          success do |option|
+            option.subject = @recipe
+            option.message = I18n.t('flash.recipe.destroyed.success')
+          end
+        else
+          failure do |option|
+            option.subject = @recipe
+            option.message = I18n.t('flash.recipe.destroyed.failure')
+          end
+        end
+      else
+        not_allowed! unless member_owns_recipe?
+        already_published! unless recipe_not_published?
+      end
+    end
+    #endregion
+
+    #region Private
+    def not_allowed!
+      failure do |option|
+        option.subject = @recipe
+        option.message = I18n.t('flash.recipe.common.not_allowed')
+      end
+    end
+
+    def already_published!
+      failure do |option|
+        option.subject = @recipe
+        option.message = I18n.t('flash.recipe.common.already_published')
+      end
+    end
+
+    def can_destroy?
+      return true if member_owns_recipe? && recipe_not_published?
+    end
+
+    def member_owns_recipe?
+      @recipe.owned_by?(@member)
+    end
+
+    def recipe_not_published?
+      !@recipe.published?
+    end
+    #endregion
+  end
+end
