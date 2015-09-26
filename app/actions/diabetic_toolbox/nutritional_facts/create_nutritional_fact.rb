9e8f3faf75a8beb972f6e80059a7e493cf317df1
bewitchingme <rpc@bewitching.me>
2015-09-26 04:06:34 -0400

    Action class to create a NutritionalFact

---
+++ app/actions/diabetic_toolbox/nutritional_facts/create_nutritional_fact.rb
@@ -0,0 +1,62 @@
+module DiabeticToolbox
+  rely_on :action
+  
+  class CreateNutritionalFact < Action
+    #region Init
+    def initialize(member, recipe, nutritional_fact_params)
+      @member, @recipe, @nutritional_fact = member, recipe, recipe.nutritional_facts.new( nutritional_fact_params )
+    end
+    #endregion
+
+    #region Protected
+    protected
+    def _call
+      if can_create?
+        if @nutritional_fact.save
+          success do |option|
+            option.subject = @nutritional_fact
+            option.message = I18n.t('flash.nutritional_fact.created.success')
+          end
+        else
+          failure do |option|
+            option.subject = @nutritional_fact
+            option.message = I18n.t('flash.nutritional_fact.created.failure')
+          end
+        end
+      else
+        not_allowed! unless member_owns_recipe?
+        already_published! unless recipe_not_published?
+      end
+    end
+    #endregion
+
+    #region Private
+    private
+    def not_allowed!
+      failure do |option|
+        option.subject = @nutritional_fact
+        option.message = I18n.t('flash.recipe.common.not_allowed')
+      end
+    end
+
+    def already_published!
+      failure do |option|
+        option.subject = @nutritional_fact
+        option.message = I18n.t('flash.recipe.common.already_published')
+      end
+    end
+
+    def can_create?
+      member_owns_recipe? && recipe_not_published?
+    end
+
+    def member_owns_recipe?
+      @recipe.owned_by? @member
+    end
+
+    def recipe_not_published?
+      !@recipe.published?
+    end
+    #endregion
+  end
+end
