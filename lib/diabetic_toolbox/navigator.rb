67919ccaa46bf0534dae918f89e758fad90756be
bewitchingme <rpc@bewitching.me>
2015-10-01 18:16:33 -0400

    Added documentation for this module.

--- lib/diabetic_toolbox/navigator.rb
+++ lib/diabetic_toolbox/navigator.rb
@@ -1,6 +1,28 @@
 module DiabeticToolbox
-  #:enddoc:
+  # = Navigator
+  #
+  # The Navigator module assists in storing navigation.  It assumes that
+  # top-level navigation elements (waypoints on the pathway) will be
+  # paired with an icon but that rest stops will not.  As follows:
+  #
+  #   Navigator.chart :members do |pathway|
+  #     pathway.waypoint :sym, 'Label', 'icon', helper_path do |stop|
+  #       stop.attraction :sub_sym, 'Label', helper_path
+  #     end
+  #   end
+  #
+  #   Navigator.course(:members).waypoints.each do |waypoint,anchor|
+  #     # waypoint       = :sym
+  #     # anchor[:sign]  = 'Label'
+  #     # anchor[:icon]  = 'icon'
+  #     # anchor[:route] = helper_path
+  #   end
+  #
+  # See: DiabeticToolbox::ApplicationController#deploy_member_navigation
+  #
   module Navigator
+    #:enddoc:
+    #region Module Methods
     def self.chart(destination, &block)
       @pathways ||= {}
 
@@ -13,7 +35,9 @@ module DiabeticToolbox
     def self.course(destination)
       @pathways[destination]
     end
+    #endregion
 
+    #region Pathway
     class Pathway
       def initialize
         @waypoints  = {}
@@ -37,7 +61,9 @@ module DiabeticToolbox
         end
       end
     end
+    #endregion
 
+    #region Rest Stop
     class RestStop
       def initialize
         @locations = {}
@@ -51,5 +77,6 @@ module DiabeticToolbox
         @locations
       end
     end
+    #endregion
   end
 end
